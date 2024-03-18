cf489607e44b21c6c8dfb6db0ae3eeafbe24003d
Jorge Miguel <campusanojorge743@gmail.com>
2024-03-18 00:43:06 -0400

    Repositorio Creado

---
+++ ClimaAPP/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClimaAPP
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "12e717669f6d0f7358f106ac8ab9528b";
+    }
+}
---
+++ ClimaAPP/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace ClimaAPP
+{
+    public partial class MainPage : ContentPage
+    {
+        RestService _resService;
+        public MainPage()
+        {
+            InitializeComponent();
+            _resService = new RestService();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(cityEntry.Text))
+            {
+                WeatherData weatherData = await
+                    _resService.GetWeatherDataAsync(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+                BindingContext = weatherData;
+            }
+        }
+
+        string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?q={cityEntry.Text}";
+            requestUri += "&units=imperial"; // or units=metric
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
+
+        }
+    }
+}
